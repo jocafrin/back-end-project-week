c9b161d45e3f431c32bb7881f2f1ca4fd84715a3
JoCatherine Griggs <jocafrin@gmail.com>
2018-09-27 14:00:58 -0400

    Completed Error handling, setting up for deployment

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const db = require("./data/db");
-const cors = require('cors');
+const cors = require("cors");
 const server = express();
 
 const corsOptions = {
-    origin: 'http://localhost:3000',
-  };
+  origin: "http://localhost:3000"
+};
 
 server.use(express.json());
 server.use(cors(corsOptions));
@@ -26,7 +26,11 @@ server.get("/notes/:id", async (req, res) => {
   const { id } = req.params;
   try {
     const note = await db("notes").where({ id });
-    res.status(200).json(note);
+    if (note.length === 0) {
+      res.status(404).send({ error: "This note does not exist" });
+    } else {
+      res.status(200).json(note);
+    }
   } catch (err) {
     console.log("/notes GET error:", err);
     res
@@ -55,38 +59,44 @@ server.post("/notes", async (req, res) => {
   }
 });
 
-server.put("/notes/:id", async (req, res) => {
+server.put("/notes/edit/:id", async (req, res) => {
   const changes = req.body;
   const { id } = req.params;
   const { title, content } = changes;
   try {
-    await db("notes")
+    const count = await db("notes")
       .where({ id })
       .update(changes);
-    res
-      .status(200)
-      .json({
+
+    if (count === 0) {
+      res.status(404).send({ error: "This note does not exist" });
+    } else {
+      res.status(200).json({
         msg: `Note ${id} has successfully been updated as:`,
         title,
         content
       });
+    }
   } catch (err) {
     console.log("/notes PUT error:", err);
-    res
-      .status(500)
-      .send({
-        error: "Your note could not be updated. Please try again later."
-      });
+    res.status(500).send({
+      error: "Your note could not be updated. Please try again later."
+    });
   }
 });
 
 server.delete("/notes/:id", async (req, res) => {
   const { id } = req.params;
   try {
-    await db("notes")
+    const count = await db("notes")
       .where({ id })
       .del();
-    res.status(200).json({ msg: `Note #${id} has been deleted.` });
+
+    if (count === 0) {
+      res.status(404).send({ error: "This note does not exist" });
+    } else {
+      res.status(200).json({ msg: `Note #${id} has been deleted.` });
+    }
   } catch (err) {
     console.log("/notes DELETE error:", err);
     res.status(500).send({ error: "Your note could not be deleted." });
